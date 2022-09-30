3c1927ec9bd5eefdc723b46d3b7df521771a5d9e
Akilesh Tangella <saiakilesh@gmail.com>
2022-09-30 11:43:42 -0400

    add clone to error

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 pub type FsDkrResult<T> = Result<T, FsDkrError>;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Clone)]
 pub enum FsDkrError {
     #[error("Too many malicious parties detected! Threshold {threshold:?}, Number of Refreshed Messages: {refreshed_keys:?}, Malicious parties detected when trying to refresh: malicious_parties:?")]
     PartiesThresholdViolation {
